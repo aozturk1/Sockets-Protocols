531941b8db5d435795c8d4d05a6f01180ec18e44
aozturk1 <aozturk1@asu.edu>
2023-01-18 23:42:09 -0700

    Added JavaGradle folder

---
+++ Assignment 1/JavaGradle/src/main/java/Fraction.java
@@ -0,0 +1,88 @@
+import java.io.*;
+/**
+ * Purpose: demonstrate simple Java Fraction class with command line,
+ * jdb debugging, and Ant build file.
+ *
+ * Ser321 Foundations of Distributed Applications
+ * see http://pooh.poly.asu.edu/Ser321
+ * @author Tim Lindquist Tim.Lindquist@asu.edu
+ *         Software Engineering, CIDSE, IAFSE, ASU Poly
+ * @version January 2020
+ */
+public class Fraction {
+
+   private int numerator, denominator;
+
+   public Fraction(){
+      numerator = denominator = 0;
+   }
+
+   public void print() {
+    System.out.print(numerator + "/" + denominator );
+   }
+
+   public void setNumerator (int n ){
+      numerator = n;
+   }
+
+   public void setDenominator (int d) {
+      denominator = d;
+   }
+
+   public int getDenominator() {
+      return denominator;
+   }
+
+   public int getNumerator() {
+      return numerator;
+   }
+
+   public static void main (String args[]) {
+      try {
+         // create a new instance
+         // Fraction *frac = [[Fraction alloc] init];
+         Fraction frac = new Fraction();
+
+	if (args.length == 2) {
+        int argX = 0;
+        int argY = 0;
+        try {
+          argX = Integer.parseInt(args[0]);
+          argY = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+          System.out.println("Arguments: " + args[0] + ", " + args[1] + " must be integers.");
+          System.exit(1);
+        }
+        //System.out.println(argX + " / " + argY);
+	frac.setNumerator(argX);
+	frac.setDenominator(argY);
+	} else if (args.length == 1) {
+        int argY = 0;
+        try {
+          argY = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+          System.out.println("Arguments: " + args[1] + " must be integers.");
+          System.exit(1);
+        }
+        //System.out.println(1 + " / " + argY);
+	frac.setNumerator(1);
+	frac.setDenominator(argY);
+      	} else {
+	  frac.setNumerator(1);
+          frac.setDenominator(3);
+	  System.out.println("Exactly 2 arguments should be provided.\n gradle run --args='1 3'");
+      	}
+
+         // set the values
+
+         // print it
+         System.out.print("The fraction is: ");
+         frac.print();
+         System.out.println("");
+
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
+
